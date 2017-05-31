a97cc9c217e905b2d5a1bae6dce1685640766097
Gabriel Bezerra Valério <gbrvalerio@Mac311.local>
2017-05-31 15:51:57 -0300

    Finished the createShape function of the CircularMenuButton

--- GVCircularMenu/CircularMenuButtonView.swift
+++ GVCircularMenu/CircularMenuButtonView.swift
@@ -36,7 +36,30 @@ class CircularMenuButtonView: UIView {
 extension CircularMenuButtonView {
     
     fileprivate func createShape(){
+        let radius = frame.width / 2 //half the width so it can be circular.
         
+        let path = CGMutablePath()
+        
+        path.move(to: .zero) //starting point
+        //the - 2.0 is to prevent the stroke to being "cut" when rendering
+        path.addArc(center: path.currentPoint, radius: radius - 2.0, startAngle: 0, endAngle: properties.angle, clockwise: false)
+        path.addLine(to: .zero) //ending point
+        
+        shapeLayer = CAShapeLayer()
+        
+        //the following line converts the path boundingbox (its the smaller cgrect where the path can fit) to the view (self) coordinates space.
+        shapeLayer.frame = self.convert(path.boundingBox, to: self)
+        //sets the position to the midpoint between the two.
+        shapeLayer.position = CGPoint(x: (layer.frame.size.width + shapeLayer.frame.size.width) / 2,
+                                      y: (layer.frame.size.height + shapeLayer.frame.size.height) / 2)
+        shapeLayer.path = path.copy() //the shapelayer now have the non-mutable copy of the previous created path
+        
+        //setting the colors
+        shapeLayer.strokeColor = properties.strokeColor.cgColor
+        shapeLayer.fillColor = properties.fillColor.cgColor
+        
+        //adds the shapelayer to the views layer.
+        layer.addSublayer(shapeLayer)
     }
     
 }
