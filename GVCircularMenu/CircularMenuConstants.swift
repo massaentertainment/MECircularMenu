e1c8b0531bc98097faf513b9eadd8550449ac6c9
Gabriel Bezerra Valério <gbrvalerio@Mac311.local>
2017-05-31 15:34:35 -0300

    Created constants file.

---
+++ GVCircularMenu/CircularMenuConstants.swift
@@ -0,0 +1,14 @@
+//
+//  CircularMenuConstants.swift
+//  GVCircularMenu
+//
+//  Created by Gabriel Bezerra Valério on 31/05/17.
+//  Copyright © 2017 bepiducb. All rights reserved.
+//
+
+import UIKit
+
+public let kCircularMenuButtonAngle = "Angle"
+public let kCircularMenuButtonBackgroundColor = "BgColor"
+public let kCircularMenuButtonStrokeColor = "StrokeColor"
+public let kCircularMenuButtonFillColor = "FillColor"
