618a329c4d69264157ef335a5a310808ec5418a9
Gabriel Valério <gbrvalerio@Mac311.local>
2017-05-31 15:04:21 -0300

    Initial Commit

---
+++ GVCircularMenu/GVCircularMenu.h
@@ -0,0 +1,19 @@
+//
+//  GVCircularMenu.h
+//  GVCircularMenu
+//
+//  Created by Gabriel Bezerra Valério on 31/05/17.
+//  Copyright © 2017 bepiducb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GVCircularMenu.
+FOUNDATION_EXPORT double GVCircularMenuVersionNumber;
+
+//! Project version string for GVCircularMenu.
+FOUNDATION_EXPORT const unsigned char GVCircularMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GVCircularMenu/PublicHeader.h>
+
+
