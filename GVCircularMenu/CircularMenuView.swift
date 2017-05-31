a17ed0536024985b976e5de86ab6e52660d47a4d
Gabriel Bezerra Valério <gbrvalerio@Mac311.local>
2017-05-31 17:13:24 -0300

    Created the CircularMenuView base class.

---
+++ GVCircularMenu/CircularMenuView.swift
@@ -0,0 +1,21 @@
+//
+//  CircularMenuView.swift
+//  GVCircularMenu
+//
+//  Created by Gabriel Bezerra Valério on 31/05/17.
+//  Copyright © 2017 bepiducb. All rights reserved.
+//
+
+import UIKit
+
+class CircularMenuView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
